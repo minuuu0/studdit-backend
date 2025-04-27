85cdb71b2f31b69ac42dbfd733ea439c7e93375b
tiel0043 <rlamw2000@naver.com>
2025-04-28 00:53:28 +0900

    chore: 프로젝트 기본 구조 및 의존성 설정

---
+++ src/main/java/com/studdit/StudditApplication.java
@@ -0,0 +1,13 @@
+package com.studdit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudditApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studdit/StudditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studdit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
